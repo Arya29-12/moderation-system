73b45076222dbd5da408312f561bd30600746a5f
Arya Sharma <aryasharma0709@gmail.com>
2026-03-23 22:28:32 +0530

    fix MongoDB initialization (startup event)

--- app/db/db.py
+++ app/db/db.py
@@ -3,13 +3,23 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-MONGO_URI = os.getenv("MONGO_URI")
 
-client = MongoClient(MONGO_URI)
-db = client["moderation_db"]
+client = None
+db = None
+users_collection = None
+content_collection = None
 
-users_collection = db["users"]
-content_collection = db["content"]
 
-users_collection.create_index("email", unique=True)
-content_collection.create_index("user_id")
+def connect_to_mongo():
+    global client, db, users_collection, content_collection
+
+    MONGO_URI = os.getenv("MONGO_URI")
+
+    client = MongoClient(MONGO_URI)
+    db = client["moderation_db"]
+
+    users_collection = db["users"]
+    content_collection = db["content"]
+
+    users_collection.create_index("email", unique=True)
+    content_collection.create_index("user_id")
--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from app.routes import user, content
+from app.db.db import connect_to_mongo
 
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_event():
+    connect_to_mongo()
 
 app.include_router(user.router)
 app.include_router(content.router)
 
 @app.get("/")
 def home():
-    return {"message":"API Running"}
+    return {"message": "API Running"}
