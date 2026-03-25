3032df2e285357276a5325f24d739c54bc8c643c
Arya Sharma <aryasharma0709@gmail.com>
2026-03-25 23:20:40 +0530

    Added user route

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-#from app.routes import user, content
+from app.routes import user#, content
 #from app.db.db import connect_to_mongo
 
 app = FastAPI()
 
-#app.include_router(user.router)
+app.include_router(user.router)
 #app.include_router(content.router)
 
 @app.get("/")
