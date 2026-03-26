3de951c278f942d753cd3851b45312fe78fbc336
Arya Sharma <aryasharma0709@gmail.com>
2026-03-26 18:54:51 +0530

    add:fixed main for content

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app.routes import user#, content
+from app.routes import user, content
 from app.db import db as db_module
 
 app = FastAPI()
 
 app.include_router(user.router)
-#app.include_router(content.router)
+app.include_router(content.router)
 
 @app.get("/")
 def home():
